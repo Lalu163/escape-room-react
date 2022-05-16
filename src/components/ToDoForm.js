b3d8e24b9c53d0101ec738be0735aef8fa32a594
Laura Contreras <m.laura.cp89@gmail.com>
2022-05-16 15:47:24 +0200

    adding hooks useState

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function ToDoForm(props){
+    const [input, setInput] = useState('');
     return(
         <form className='toDo-form'>
-
+            <input type="text" placeholder="Add a to do" value={input} name="text" className="toDo-input"/>
+                <button className="toDo-button">Add something to do</button>
         </form>
     )
 }
